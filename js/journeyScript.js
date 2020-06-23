9016abd745cd850f163352c99870a28af868968a
Pamela <pamela-n@adadev.org.za>
2020-06-23 15:02:24 +0200

    full pages

---
+++ js/journeyScript.js
@@ -0,0 +1,20 @@
+// Navbar section
+
+const bars = document.querySelector('.bars');
+const MoMenu = document.querySelector('.moMenu');
+const vala = document.querySelector('.time');
+const website = document.querySelector('.Webber');
+
+MoMenu.style.display = 'none'
+
+bars.addEventListener('click', () =>{
+    MoMenu.style.display = 'block';
+    website.style.display = 'none';
+
+
+});
+
+vala.addEventListener('click',()=>{
+    MoMenu.style.display = 'none';
+    website.style.display = 'block';
+});
